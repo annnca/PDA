b81a29a591dd9f3a4a1e485eaa2a326a452db09d
annnca <stanasel.anca@yahoo.com>
2018-03-04 15:25:48 +0200

    update

--- lab1/Cons.java
+++ lab1/Cons.java
@@ -2,23 +2,40 @@ package l1;
 
 import java.util.Queue;
 
-public class Cons extends Thread{
-	Queue<Integer> q;
-	
-	public Cons(Queue<Integer> q) {
-		super();
-		this.q = q;
-	}
-	public synchronized void consume(){
-		System.out.println("consumer consumed "+ q.peek());
-		q.remove();
-	}
-	public void run(){
-		while(true){
-			if(q.size() > 0){
-				consume();
-			}
-		}
-	}
 
+public class Cons extends Thread{
+
+    private Queue<Integer> q;
+
+    public Cons(Queue<Integer> q) {
+        this.q = q;
+    }
+
+    private void consume(){
+    	System.out.println("consumer consumed "+ q.peek());
+    	q.remove();
+    }
+    
+    @Override
+    public void run() {
+    	for (int i = 0; i < 7; i++) {
+        	synchronized (q) {
+        		while (q.isEmpty()) {        
+                    System.out.println("Empty queue.Consumer is waiting...");
+                    try {
+						q.wait();
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}            
+        		}
+        		
+                q.notifyAll();
+                consume();
+        	}
+
+        }
+    }
 }
+
+
