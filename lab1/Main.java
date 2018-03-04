5df1a6728ca3722356ba0d0ec06b7098e0a761ed
annnca <stanasel.anca@yahoo.com>
2018-03-04 15:26:30 +0200

    update

--- lab1/Main.java
+++ lab1/Main.java
@@ -2,26 +2,24 @@ package l1;
 
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 
 public class Main {
 	public static Queue<Integer> queue;
-	public final static Lock mtx = new ReentrantLock(true);
-	public static int maxDim;
+	/*public final static Lock mtx = new ReentrantLock(true);*/
+	public final static int maxDim = 4;
 	
 	public static void main(String[] args) throws InterruptedException {
-		queue = new LinkedList<Integer>();
-		maxDim = 5;
 		
-		Thread p = new Prod(queue);
-		Thread c = new Cons(queue);
-		
-		p.start();
-		c.start();
-		
-		p.join();
-		c.join();
-	}
-
+        queue = new LinkedList<Integer>();
+        
+        Thread p = new Thread(new Prod(queue));
+        Thread c = new Thread(new Cons(queue));
+        
+        p.start();
+        c.start();
+        
+        p.join();
+        c.join();
+    }
 }
+
